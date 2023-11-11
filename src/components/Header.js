223ebb2d7916913484cba63b1f402047d3911fc6
SRK <choudharyshravak@gmail.com>
2023-11-11 20:55:49 +0530

    signout feature and user dispatch

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,35 @@
 import React from 'react'
 import Netflix_Logo from "../images/Netflix_Logo_PMS.png"
+import userIcon from "../images/userIcon.png"
+import { signOut } from "firebase/auth";
+import {auth} from "../utils/firebase"
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const Header = () => {
+  const navigate = useNavigate();
+  const user = useSelector((store) => store.user)
+  const handleSignout = () => {
+    signOut(auth).then(() => {
+      // Sign-out successful.
+      navigate("/")
+    }).catch((error) => {
+      // An error happened.
+      navigate("/error")
+    });
+  }
   return (
-    <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10">
-      <img className="h-16 mx-2" src={Netflix_Logo} />
-    </div>
+    <>
+      <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10 w-screen flex justify-between">
+        <img className="h-16 mx-2" src={Netflix_Logo} />
+        {user && (<div className="flex p-2">
+          <img className="w-12 h-12" alt="usericon" src={userIcon} />
+          <button onClick={handleSignout} className="font-bold text-white">
+            (Sign Out)
+          </button>
+        </div>)}
+      </div>
+    </>
   )
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -2,15 +2,18 @@ import React, { useRef, useState } from 'react'
 import Header from './Header'
 import Netflix_Background from "../images/Netflix_Background.jpg"
 import { checkValidData } from "../utils/validate";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
 import {auth} from "../utils/firebase"
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
+  const navigate = useNavigate();
 
   const email = useRef(null);
   const password = useRef(null);
+  const name = useRef(null);
 
   const handleButtonClick = () => {
     const message  =checkValidData(email.current.value, password.current.value)
@@ -24,6 +27,16 @@ const Login = () => {
         .then((userCredential) => {
           // Signed up 
           const user = userCredential.user;
+          updateProfile(auth.currentUser, {
+            displayName: name.current.value, photoURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRh-3lSF_C0nNEgC1O9cSIkFDPlWLexdkxS0JQH_znhO3pHtThyHfprCnc0no2QaANtFqE&usqp=CAU"
+          }).then(() => {
+            // Profile updated!
+            // ...
+            navigate("/browse");
+          }).catch((error) => {
+            setErrorMessage(error.message)
+          });
+          
           // ...
         })
         .catch((error) => {
@@ -37,6 +50,7 @@ const Login = () => {
         .then((userCredential) => {
           // Signed in 
           const user = userCredential.user;
+          navigate("/browse")
           //..
         })
         .catch((error) => {
@@ -61,7 +75,7 @@ const Login = () => {
 
       <form onSubmit={(e) => e.preventDefault()} className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
       <h1 className="font-bold text-3xl py-2">{isSignInForm ? "Sign In" : "Sign Up"}</h1>
-        {!isSignInForm && <input type="text" placeholder="Full Name" className="p-4 my-4 w-full bg-gray-700"/>}
+        {!isSignInForm && <input ref={name} type="text" placeholder="Full Name" className="p-4 my-4 w-full bg-gray-700"/>}
         <input ref={email} type="text" placeholder="Email Address" className="p-4 my-4 w-full bg-gray-700"/>
         <input ref={password} type="password" placeholder="Password" className="p-4 my-4 w-full bg-gray-700"/>
         <p className="text-red-500 font-bold text-lg py-2">{errorMessage}</p>
