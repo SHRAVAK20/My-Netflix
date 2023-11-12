405f5e7bba9b4009ebcbdd106269fd70a15e4c64
SRK <choudharyshravak@gmail.com>
2023-11-12 13:43:45 +0530

    Fetch Popular movie list & add to 2nd'ry conatiner

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -11,9 +11,9 @@ const SecondaryContainer = () => {
     <div className="bg-black">
       <div className="-mt-48 pl-12 relative z-20">
       <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies}/>
+      <MovieList title={"Popular"} movies={movies.popularMovies} />
       <MovieList title={"Trending"} movies={movies.nowPlayingMovies}/>
       <MovieList title={"Horror"} movies={movies.nowPlayingMovies}/>
-      <MovieList title={"Popular"} movies={movies.nowPlayingMovies}/>
       </div>
     </div>
   )
