9ad223d7597a06d18915732ae37b45cc4254d25e
SRK <choudharyshravak@gmail.com>
2023-11-12 10:07:00 +0530

    fetch movies data from tmdb

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,10 @@
 export const USER_AVATAR =
   "https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
+
+  export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMWMwMzg4Njg4NzNlNjFiOWMyOGVlMjlmYzY3NDlhMiIsInN1YiI6IjY1NTA0ZGRlZDRmZTA0MDBhYzMzYjI3NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.tI4c0unNOymRDNiU5-Djj72V-0LZGiQnTyDuUGxo2dQ'
+    }
+  };
